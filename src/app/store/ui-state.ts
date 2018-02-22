1b86618d4363ba2baecaca2f01433962d2eece1b
Juri Thommen <23640912+jthommen@users.noreply.github.com>
2018-02-22 18:40:09 +0000

    Implement filter fields programmatically & add UiState on change

--- src/app/store/ui-state.ts
+++ src/app/store/ui-state.ts
@@ -4,6 +4,6 @@ export interface UiState {
 }
 
 export const INITIAL_UI_STATE: UiState = {
-    filterNeighborhoods: 'All Neighborhoods',
-    filterCuisines: 'All Cuisines'
+    filterNeighborhoods: 'All',
+    filterCuisines: 'All'
 }

97911758e603922ce7cb6a805329dcb3b5a85676
Juri Thommen <23640912+jthommen@users.noreply.github.com>
2018-03-01 11:59:53 +0000

    Create cloud firestore data seed process

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD7mmudqxRJIDDxuQ28M0UxuNXmDcWKcMg",
+    authDomain: "restaurant-reviewapp.firebaseapp.com",
+    databaseURL: "https://restaurant-reviewapp.firebaseio.com",
+    projectId: "restaurant-reviewapp",
+    storageBucket: "restaurant-reviewapp.appspot.com",
+    messagingSenderId: "19402762324"
+  }
 };
